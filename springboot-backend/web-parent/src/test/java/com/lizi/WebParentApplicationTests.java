62727d67e79d709b8e0a109925c7c317fe7d2b7a
khoahc <khoahc98@gmail.com>
2022-09-02 18:29:29 +0700

    created project

---
+++ springboot-backend/web-parent/src/test/java/com/lizi/WebParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
