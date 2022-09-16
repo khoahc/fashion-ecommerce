82f77d7f7cc83257ca7fe1bb60b778cd0fbd4010
khoahc <khoahc98@gmail.com>
2022-09-16 14:35:32 +0700

    init components, pages, sass

---
+++ react-frontend/customer-app/src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Router } from "react-router-dom";
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = () => {
+  return <h1>Layout</h1>;
+};
+
+export default Layout;
