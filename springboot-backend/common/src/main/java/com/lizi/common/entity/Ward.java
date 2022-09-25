ec005b1f91545c61fc06aedd2f6a80b8b4aa47ee
Võ Văn Hậu <vovanhauspkt@gmail.com>
2022-09-25 19:26:52 +0700

    add some new entity about address

---
+++ springboot-backend/common/src/main/java/com/lizi/common/entity/Ward.java
@@ -0,0 +1,39 @@
+package com.lizi.common.entity;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_wards")
+public class Ward {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 20, nullable = false, unique = true)
+  private String name;
+
+  private BigDecimal shipCost;
+
+  private int shipTime;
+
+  @ManyToOne
+  @JoinColumn(name = "district_id")
+  private District district;
+}
