ed545e3d652f40cba4bfa021609e4a516406a5d9
Võ Văn Hậu <vovanhauspkt@gmail.com>
2022-10-03 01:35:29 +0700

    [backend-cms] - add some new entity (ProductColor, ProductImageColor)

---
+++ springboot-backend/common/src/main/java/com/lizi/common/entity/ProductColor.java
@@ -0,0 +1,41 @@
+package com.lizi.common.entity;
+
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_product_colors")
+public class ProductColor {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String hexCode;
+
+  @OneToOne
+  @JoinColumn(name = "main_image_id")
+  private Image mainImage;
+
+  @OneToMany(mappedBy = "productColor")
+  private Set<ProductImageColor> productImageColors;
+}
