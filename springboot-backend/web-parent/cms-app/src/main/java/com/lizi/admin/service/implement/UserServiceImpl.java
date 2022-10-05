7b33350a1d9fe45196ca33f48907f9c84d4f165a
Võ Văn Hậu <vovanhauspkt@gmail.com>
2022-10-05 23:13:01 +0700

    [backend-cms] - update service create and update user

--- springboot-backend/web-parent/cms-app/src/main/java/com/lizi/admin/service/implement/UserServiceImpl.java
+++ springboot-backend/web-parent/cms-app/src/main/java/com/lizi/admin/service/implement/UserServiceImpl.java
@@ -5,11 +5,16 @@ import com.lizi.admin.dto.user.UserResDto;
 import com.lizi.admin.exception.ResourceAlreadyExistsException;
 import com.lizi.admin.exception.ResourceNotFoundException;
 import com.lizi.admin.mapper.UserMapper;
+import com.lizi.admin.repository.ImageRepository;
+import com.lizi.admin.repository.RoleRepository;
 import com.lizi.admin.repository.UserRepository;
 import com.lizi.admin.service.UserService;
+import com.lizi.common.entity.Image;
+import com.lizi.common.entity.Role;
 import com.lizi.common.entity.User;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +26,12 @@ public class UserServiceImpl implements UserService {
 
   @Autowired
   private UserRepository userRepo;
+
+  @Autowired
+  private ImageRepository imageRepo;
+
+  @Autowired
+  private RoleRepository roleRepo;
 
   @Autowired
   private BCryptPasswordEncoder passwordEncoder;
@@ -44,8 +55,17 @@ public class UserServiceImpl implements UserService {
       throw new ResourceAlreadyExistsException("user", "email", userReqDto.getEmail());
     }
 
+    Image photo = imageRepo.findById(userReqDto.getPhotoId())
+        .orElseThrow(() -> new ResourceNotFoundException("image", "id", userReqDto.getPhotoId()));
+
+    Set<Role> roles = userReqDto.getRoleIds().stream().map(rId -> roleRepo.findById(rId)
+            .orElseThrow(() -> new ResourceNotFoundException("role", "id", rId)))
+        .collect(Collectors.toSet());
+
     User newUser = UserMapper.INSTANCE.dtoToUser(userReqDto);
     newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+    newUser.setPhoto(photo);
+    newUser.setRoles(roles);
 
     return UserMapper.INSTANCE.userToDto(userRepo.saveAndFlush(newUser));
   }
@@ -53,29 +73,33 @@ public class UserServiceImpl implements UserService {
   @Override
   @Transactional
   public UserResDto updateUser(Long id, UserReqDto userReqDto) {
-    Optional<User> userGetByIdOptional = userRepo.findById(id);
+    User user = userRepo.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("user", "id", id));
 
-    if (userGetByIdOptional.isEmpty()) {
-      throw new ResourceNotFoundException("user", "id", id);
-    }
+    Optional<User> userBeGetByEmailOptional = userRepo.findByEmail(userReqDto.getEmail());
+    if (userBeGetByEmailOptional.isPresent()) {
+      User userBeGetByEmail = userBeGetByEmailOptional.get();
 
-    Optional<User> userGetByEmailOptional = userRepo.findByEmail(userReqDto.getEmail());
-    if (userGetByEmailOptional.isPresent()) {
-      if (!userGetByEmailOptional.equals(userGetByEmailOptional)) {
+      if (!user.equals(userBeGetByEmail)) {
         throw new ResourceAlreadyExistsException("user", "email", userReqDto.getEmail());
       }
     }
 
-    User user = userGetByIdOptional.get();
-    User dataUpdate = UserMapper.INSTANCE.dtoToUser(userReqDto);
+    Image photo = imageRepo.findById(userReqDto.getPhotoId())
+        .orElseThrow(() -> new ResourceNotFoundException("image", "id", userReqDto.getPhotoId()));
 
-    user.setEmail(dataUpdate.getEmail());
-    user.setPassword(passwordEncoder.encode(dataUpdate.getPassword()));
-    user.setFirstName(dataUpdate.getFirstName());
-    user.setLastName(dataUpdate.getLastName());
-    user.setPhoto(dataUpdate.getPhoto());
-    user.setEnabled(dataUpdate.isEnabled());
-    user.setRoles(dataUpdate.getRoles());
+    Set<Role> roles = userReqDto.getRoleIds().stream().map(rId -> roleRepo.findById(rId)
+            .orElseThrow(() -> new ResourceNotFoundException("role", "id", rId)))
+        .collect(Collectors.toSet());
+
+    user.setEmail(userReqDto.getEmail());
+    user.setPassword(passwordEncoder.encode(userReqDto.getPassword()));
+    user.setFirstName(userReqDto.getFirstName());
+    user.setLastName(userReqDto.getLastName());
+    user.setEnabled(userReqDto.isEnabled());
+
+    user.setPhoto(photo);
+    user.setRoles(roles);
 
     return UserMapper.INSTANCE.userToDto(userRepo.save(user));
   }
