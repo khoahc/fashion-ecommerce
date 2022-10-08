2b84f2c5324deda145e398956fdf21ee9c9c171d
khoahc <khoahc98@gmail.com>
2022-10-08 18:27:59 +0700

    [backend-customer] - update appication.yml file

--- springboot-backend/web-parent/customer-app/src/main/java/com/lizi/customer/CustomerAppApplication.java
+++ springboot-backend/web-parent/customer-app/src/main/java/com/lizi/customer/CustomerAppApplication.java
@@ -2,8 +2,11 @@ package com.lizi.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EntityScan({"com.lizi.common.entity"})
 public class CustomerAppApplication {
 
 	public static void main(String[] args) {
