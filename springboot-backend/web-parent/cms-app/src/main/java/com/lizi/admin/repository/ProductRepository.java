3544236b57dfb64f2d6dc579cc2060c54489ba1d
Võ Văn Hậu <vovanhauspkt@gmail.com>
2022-10-09 03:21:47 +0700

    [backend-cms] - update services create, update product

--- springboot-backend/web-parent/cms-app/src/main/java/com/lizi/admin/repository/ProductRepository.java
+++ springboot-backend/web-parent/cms-app/src/main/java/com/lizi/admin/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.lizi.admin.repository;
 
 import com.lizi.common.entity.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+  Optional<Product> findByName(String name);
+  Optional<Product> findBySlug(String slug);
 }
