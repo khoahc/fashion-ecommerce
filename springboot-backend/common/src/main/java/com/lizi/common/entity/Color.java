d5e875b531340307848a99c2c7c4b63cad88ceb5
Võ Văn Hậu <vovanhauspkt@gmail.com>
2022-10-15 23:56:41 +0700

    [backend-enity] - add color enity

--- springboot-backend/common/src/main/java/com/lizi/common/entity/Color.java
+++ springboot-backend/common/src/main/java/com/lizi/common/entity/Color.java
@@ -1,14 +1,10 @@
 package com.lizi.common.entity;
 
-import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,21 +18,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "tbl_product_colors")
-public class ProductColor {
+@Table(name = "tbl_color")
+public class Color {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  private String name;
 
-  @ManyToOne
-  @JoinColumn(name = "color_id")
-  private Color color;
+  @Column(length = 128, nullable = false)
+  private String slug;
 
-  @OneToOne
-  @JoinColumn(name = "main_image_id")
-  private Image mainImage;
-
-  @OneToMany(mappedBy = "productColor")
-  private Set<ProductImageColor> productImageColors;
+  private String hexCode;
 }
