20a7d9485006f2d6e0751207834dd174e120a6c6
Võ Văn Hậu <vovanhauspkt@gmail.com>
2022-11-03 23:58:00 +0700

    [backend-cms] - add get all order api

---
+++ springboot-backend/web-parent/cms-app/src/main/java/com/lizi/admin/dto/order/OrderResDto.java
@@ -0,0 +1,33 @@
+package com.lizi.admin.dto.order;
+
+import com.lizi.admin.dto.customer.CustomerResDto;
+import com.lizi.admin.dto.voucher.VoucherResDto;
+import java.math.BigDecimal;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderResDto {
+
+  private Long id;
+  private String receiverName;
+  private String phoneNumber;
+  private String email;
+  private String address;
+  private Date orderTime;
+  private BigDecimal shipCost;
+  private BigDecimal subtotal;
+  private BigDecimal totalPrice;
+  private String paymentMethod;
+  private CustomerResDto customer;
+  private VoucherResDto voucher;
+
+}
