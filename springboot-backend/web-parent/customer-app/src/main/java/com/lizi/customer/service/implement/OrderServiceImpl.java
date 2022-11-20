9a2c49f370cd4481d0328dbe089eb508d52a7436
khoahc <khoahc98@gmail.com>
2022-11-20 22:51:54 +0700

    [backend-customer] - update api for add order

--- springboot-backend/web-parent/customer-app/src/main/java/com/lizi/customer/service/implement/OrderServiceImpl.java
+++ springboot-backend/web-parent/customer-app/src/main/java/com/lizi/customer/service/implement/OrderServiceImpl.java
@@ -82,74 +82,140 @@ public class OrderServiceImpl implements OrderService {
     String toAddress = order.getEmail();
     String fromAddress = "phongnha0167@gmail.com";
     String senderName = "Lizi";
-    String subject = "[LIZI] - Xác nhận đơn hàng của bạn";
+//    String subject = "[LIZI] - Xác nhận đơn hàng của bạn";
+//    String content =
+//    "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+//            "\n" +
+//            "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+//            "\n" +
+//            "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+//            "    <tbody><tr>\n" +
+//            "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+//            "        \n" +
+//            "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+//            "          <tbody><tr>\n" +
+//            "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+//            "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;margin:auto\">\n" +
+//            "                  <tbody><tr>\n" +
+//            "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px\">\n" +
+//            "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Xác nhận đơn hàng</span>\n" +
+//            "                    </td>\n" +
+//            "                  </tr>\n" +
+//            "                </tbody></table>\n" +
+//            "              </a>\n" +
+//            "            </td>\n" +
+//            "          </tr>\n" +
+//            "        </tbody></table>\n" +
+//            "        \n" +
+//            "      </td>\n" +
+//            "    </tr>\n" +
+//            "  </tbody></table>\n" +
+//            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+//            "    <tbody><tr>\n" +
+//            "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+//            "      <td>\n" +
+//            "        \n" +
+//            "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+//            "                  <tbody><tr>\n" +
+//            "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+//            "                  </tr>\n" +
+//            "                </tbody></table>\n" +
+//            "        \n" +
+//            "      </td>\n" +
+//            "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+//            "    </tr>\n" +
+//            "  </tbody></table>\n" +
+//            "\n" +
+//            "\n" +
+//            "\n" +
+//            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+//            "    <tbody><tr>\n" +
+//            "      <td height=\"30\"><br></td>\n" +
+//            "    </tr>\n" +
+//            "    <tr>\n" +
+//            "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+//            "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+//            "        \n" +
+//            "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Xin chào [[name]],</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Cảm ơn bạn đã đặt mua sản phẩm của chúng tôi.</p> " +
+//            "            <p>Mã đơn hàng của bạn là: <span style=\"font-weight: bold;\">[[orderId]]</span></p>\n" +
+//            "            <p>Vui lòng click vào link dưới để xác nhận đơn hàng của bạn.</p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"[[URL]]\">Xác nhận đơn hàng</a> </p></blockquote>\n Link sẽ hết hạn sau 15 phút. <p>Cảm ơn bạn</p>" +
+//            "        \n" +
+//            "      </td>\n" +
+//            "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+//            "    </tr>\n" +
+//            "    <tr>\n" +
+//            "      <td height=\"30\"><br></td>\n" +
+//            "    </tr>\n" +
+//            "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+//            "\n" +
+//            "</div></div>\n";
+    String subject = "[LIZI] - Thông tin đơn hàng của bạn";
     String content =
-    "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
-            "\n" +
-            "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
-            "\n" +
-            "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
-            "    <tbody><tr>\n" +
-            "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
-            "        \n" +
-            "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
-            "          <tbody><tr>\n" +
-            "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
-            "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;margin:auto\">\n" +
-            "                  <tbody><tr>\n" +
-            "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px\">\n" +
-            "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Xác nhận đơn hàng</span>\n" +
-            "                    </td>\n" +
-            "                  </tr>\n" +
-            "                </tbody></table>\n" +
-            "              </a>\n" +
-            "            </td>\n" +
-            "          </tr>\n" +
-            "        </tbody></table>\n" +
-            "        \n" +
-            "      </td>\n" +
-            "    </tr>\n" +
-            "  </tbody></table>\n" +
-            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-            "    <tbody><tr>\n" +
-            "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
-            "      <td>\n" +
-            "        \n" +
-            "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-            "                  <tbody><tr>\n" +
-            "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
-            "                  </tr>\n" +
-            "                </tbody></table>\n" +
-            "        \n" +
-            "      </td>\n" +
-            "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
-            "    </tr>\n" +
-            "  </tbody></table>\n" +
-            "\n" +
-            "\n" +
-            "\n" +
-            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-            "    <tbody><tr>\n" +
-            "      <td height=\"30\"><br></td>\n" +
-            "    </tr>\n" +
-            "    <tr>\n" +
-            "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-            "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
-            "        \n" +
-            "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Xin chào [[name]],</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Cảm ơn bạn đã đặt mua sản phẩm của chúng tôi.</p> " +
-            "            <p>Mã đơn hàng của bạn là: <span style=\"font-weight: bold;\">[[orderId]]</span></p>\n" +
-            "            <p>Làm ơn click vào link dưới để xác nhận đơn hàng của bạn.</p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"[[URL]]\">Xác nhận đơn hàng</a> </p></blockquote>\n Link sẽ hết hạn sau 15 phút. <p>Cảm ơn bạn</p>" +
-            "        \n" +
-            "      </td>\n" +
-            "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-            "    </tr>\n" +
-            "    <tr>\n" +
-            "      <td height=\"30\"><br></td>\n" +
-            "    </tr>\n" +
-            "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
-            "\n" +
-            "</div></div>\n";
-
+            "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+                    "\n" +
+                    "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                    "\n" +
+                    "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                    "    <tbody><tr>\n" +
+                    "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                    "        \n" +
+                    "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                    "          <tbody><tr>\n" +
+                    "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                    "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;margin:auto\">\n" +
+                    "                  <tbody><tr>\n" +
+                    "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px\">\n" +
+                    "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Xác nhận đơn hàng</span>\n" +
+                    "                    </td>\n" +
+                    "                  </tr>\n" +
+                    "                </tbody></table>\n" +
+                    "              </a>\n" +
+                    "            </td>\n" +
+                    "          </tr>\n" +
+                    "        </tbody></table>\n" +
+                    "        \n" +
+                    "      </td>\n" +
+                    "    </tr>\n" +
+                    "  </tbody></table>\n" +
+                    "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                    "    <tbody><tr>\n" +
+                    "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                    "      <td>\n" +
+                    "        \n" +
+                    "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                    "                  <tbody><tr>\n" +
+                    "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                    "                  </tr>\n" +
+                    "                </tbody></table>\n" +
+                    "        \n" +
+                    "      </td>\n" +
+                    "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                    "    </tr>\n" +
+                    "  </tbody></table>\n" +
+                    "\n" +
+                    "\n" +
+                    "\n" +
+                    "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                    "    <tbody><tr>\n" +
+                    "      <td height=\"30\"><br></td>\n" +
+                    "    </tr>\n" +
+                    "    <tr>\n" +
+                    "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                    "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                    "        \n" +
+                    "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Xin chào [[name]],</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Cảm ơn bạn đã đặt mua sản phẩm của chúng tôi.</p> " +
+                    "            <p>Mã đơn hàng của bạn là: <span style=\"font-weight: bold;\">[[orderId]]</span></p>\n" +
+                    "            <p>Cảm ơn bạn</p>" +
+                    "        \n" +
+                    "      </td>\n" +
+                    "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                    "    </tr>\n" +
+                    "    <tr>\n" +
+                    "      <td height=\"30\"><br></td>\n" +
+                    "    </tr>\n" +
+                    "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                    "\n" +
+                    "</div></div>\n";
 
     MimeMessage message = mailSender.createMimeMessage();
     MimeMessageHelper helper = new MimeMessageHelper(message);
